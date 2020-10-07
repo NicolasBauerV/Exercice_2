1a46743f101cd1acb0270a52c9694900ca3af1ab
Nicolas Bauer <bauer.nicolas266@gmail.com>
2020-10-07 12:35:33 +0200

    Initial Commit

---
+++ Exercice_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Exercice_2
+//
+//  Created by Nicolas BAUER on 07/10/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
